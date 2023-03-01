261202fa7f38d966f53efb98e2a81e024f455fd4
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-03-01 11:40:39 +0800

    a

--- app/layout.js
+++ app/layout.js
@@ -36,15 +36,7 @@ export default async function RootLayout({ children }) {
         href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
         rel="stylesheet"
       />
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-JEYTX1BWRH"
-      ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-JEYTX1BWRH');
-      </script>
+
       <head />
       <body>
         {" "}
